d99052fdd9504d91130dc78cfc0d165d3e93dacc
DanielPenalosa <daniel@ipila.com>
2026-06-07 14:55:26 +0800

    HOTFIX: Restore home_screen.dart to working state for deployment

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
@@ -10,7 +9,6 @@ import '../../../data/models/report_model.dart';
 import '../../../data/services/report_service.dart';
 import '../../auth/providers/auth_provider.dart';
 import '../widgets/mobile_shell.dart';
-import '../widgets/web_resident_shell.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -35,18 +33,21 @@ class HomeScreen extends StatelessWidget {
             .join() ??
         'R';
 
-    final content = SingleChildScrollView(
-      padding: EdgeInsets.fromLTRB(20, 8, 20, kIsWeb ? 40 : 100),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Greeting row
-          Row(
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
+    return MobileShell(
+      title: 'iPILA',
+      currentIndex: 0,
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.fromLTRB(20, 8, 20, 100),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Greeting row
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
                       Text(
                         '${_greeting()}, $firstName',
                         style: const TextStyle(
